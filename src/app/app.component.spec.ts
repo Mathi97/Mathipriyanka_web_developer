f605a9ed386657a5c57be2107708f9f4d97bea30
Mathipriyanka T <mathi19ab@gmail.com>
2023-04-09 19:50:42 +0530

    version upgrade

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Mathipriyanka'`, () => {
+  it(`should have as title 'movie-ticketing-system'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Mathipriyanka');
+    expect(app.title).toEqual('movie-ticketing-system');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Mathipriyanka app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('movie-ticketing-system app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Mathipriyanka';
+  title = 'movie-ticketing-system';
 }
